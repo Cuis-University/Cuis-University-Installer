27ae0fb63a7408479384cd91def84c140ba43ec0
Hernan Wilkinson <hernan.wilkinson@10pines.com>
2019-02-06 16:09:20 -0300

    removal of packages before copying it and takes aconcagua/chalten from new directories

--- 1-refreshEnvironment.sh
+++ 1-refreshEnvironment.sh
@@ -4,6 +4,25 @@ rm Cuis5.0-*.image
 rm Cuis5.0-*.changes
 rm CuisUniversity*.image
 rm CuisUniversity*.changes
+
+rm -R linux32/Packages
+rm -R linux64/Packages
+rm -R windows32/Packages
+rm -R windows64/Packages
+rm -R macos64/Packages
+
+rm -R linux32/Documentation
+rm -R linux64/Documentation
+rm -R windows32/Documentation
+rm -R windows64/Documentation
+rm -R macos64/Documentation
+
+rm -R linux32/CoreUpdates
+rm -R linux64/CoreUpdates
+rm -R windows32/CoreUpdates
+rm -R windows64/CoreUpdates
+rm -R macos64/CoreUpdates
+
 cd ../Cuis-Smalltalk-Dev
 git pull
 cp Cuis5.0-*.image ../Cuis-University-Installer
@@ -15,17 +34,17 @@ cp -R Packages ../Cuis-University-Installer/windows32/Packages
 cp -R Packages ../Cuis-University-Installer/windows64/Packages
 cp -R Packages ../Cuis-University-Installer/macos64/Packages
 
-cp ../Cuis-Smalltalk-Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/linux32/Packages
-cp ../Cuis-Smalltalk-Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/linux64/Packages
-cp ../Cuis-Smalltalk-Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/windows32/Packages
-cp ../Cuis-Smalltalk-Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/windows64/Packages
-cp ../Cuis-Smalltalk-Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/macos64/Packages
+cp ../Measures/Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/linux32/Packages
+cp ../Measures/Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/linux64/Packages
+cp ../Measures/Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/windows32/Packages
+cp ../Measures/Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/windows64/Packages
+cp ../Measures/Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/macos64/Packages
 
-cp ../Cuis-Smalltalk-Chalten/Chalten.pck.st ../Cuis-University-Installer/linux32/Packages
-cp ../Cuis-Smalltalk-Chalten/Chalten.pck.st ../Cuis-University-Installer/linux64/Packages
-cp ../Cuis-Smalltalk-Chalten/Chalten.pck.st ../Cuis-University-Installer/windows32/Packages
-cp ../Cuis-Smalltalk-Chalten/Chalten.pck.st ../Cuis-University-Installer/windows64/Packages
-cp ../Cuis-Smalltalk-Chalten/Chalten.pck.st ../Cuis-University-Installer/macos64/Packages
+cp ../Calendars/Chalten/Chalten.pck.st ../Cuis-University-Installer/linux32/Packages
+cp ../Calendars/Chalten/Chalten.pck.st ../Cuis-University-Installer/linux64/Packages
+cp ../Calendars/Chalten/Chalten.pck.st ../Cuis-University-Installer/windows32/Packages
+cp ../Calendars/Chalten/Chalten.pck.st ../Cuis-University-Installer/windows64/Packages
+cp ../Calendars/Chalten/Chalten.pck.st ../Cuis-University-Installer/macos64/Packages
 
 cp ../Cuis-Smalltalk-DenotativeObject/DenotativeObject.pck.st ../Cuis-University-Installer/linux32/Packages
 cp ../Cuis-Smalltalk-DenotativeObject/DenotativeObject.pck.st ../Cuis-University-Installer/linux64/Packages
