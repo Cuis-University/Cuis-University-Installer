ecbf6f45086cb31144cd18cf4d7986563822fcc2
hernan.wilkinson <hernan.wilkinson@10pines.com>
2021-03-09 16:12:21 -0300

    Cambios para incluir VM jit, paquetes nuevos, etc

--- 1-refreshEnvironment.sh
+++ 1-refreshEnvironment.sh
@@ -28,42 +28,58 @@ rm Cuis5.0-*.image
 rm Cuis5.0-*.changes
 rm CuisUniversity*.image
 rm CuisUniversity*.changes
+rm TheCuisBook.pdf
 
-rm -R linux32/Packages
-rm -R linux64/Packages
-rm -R windows32/Packages
-rm -R windows64/Packages
-rm -R macos64/Packages
+rm -R -f linux32/Packages
+rm -R -f linux64/Packages
+rm -R -f windows32/Packages
+rm -R -f windows64/Packages
+rm -R -f macos64/Packages
 
-rm -R linux32/Documentation
-rm -R linux64/Documentation
-rm -R windows32/Documentation
-rm -R windows64/Documentation
-rm -R macos64/Documentation
+rm -R -f linux32/Documentation
+rm -R -f linux64/Documentation
+rm -R -f windows32/Documentation
+rm -R -f windows64/Documentation
+rm -R -f macos64/Documentation
 
-rm -R linux32/CoreUpdates
-rm -R linux64/CoreUpdates
-rm -R windows32/CoreUpdates
-rm -R windows64/CoreUpdates
-rm -R macos64/CoreUpdates
+rm -R -f linux32/CoreUpdates
+rm -R -f linux64/CoreUpdates
+rm -R -f windows32/CoreUpdates
+rm -R -f windows64/CoreUpdates
+rm -R -f macos64/CoreUpdates
 
-rm -R linux32/AdditionalFontData
-rm -R linux64/AdditionalFontData
-rm -R windows32/AdditionalFontData
-rm -R windows64/AdditionalFontData
-rm -R macos64/AdditionalFontData
+rm -R -f linux32/TrueTypeFonts
+rm -R -f linux64/TrueTypeFonts
+rm -R -f windows32/TrueTypeFonts
+rm -R -f windows64/TrueTypeFonts
+rm -R -f macos64/TrueTypeFonts
+
+curl -L -o TheCuisBook.pdf https://github.com/Cuis-Smalltalk/TheCuisBook/releases/download/latestpdfbuild/TheCuisBook.pdf
+cp TheCuisBook.pdf macos64
+cp TheCuisBook.pdf linux64
+cp TheCuisBook.pdf windows64
+cp TheCuisBook.pdf linux32
+cp TheCuisBook.pdf windows32
 
 cd ../Cuis-Smalltalk-Refactoring
 git pull
 
-# cd ../Cuis-Smalltalk-Refactorings-Thesis
-# git pull
+cd ../TDDGuru
+git pull
 
-# cd ../TDDGuru
-# git pull
+cd ../cuis-finder
+git pull
+
+cd ../cuis-code-coverage
+git pull
+
+cd ../cuis-system-updater
+git pull
 
 cd ../Cuis-Smalltalk-Dev
 git pull
+
+./pullAllRepos.sh
 
 cd ../Installer
 ../Squeak.app/Contents/MacOS/Squeak InstallerBuilder.image -d 'CuisInstallerBuilder generateUpdateImagesScriptAndExit'
@@ -76,71 +92,125 @@ chmod +x copy32ImageFromDev.sh
 
 cd ../Cuis-Smalltalk-Dev
 
+cp pullAllRepos.sh ../Cuis-University-Installer/linux32
+cp pullAllRepos.sh ../Cuis-University-Installer/linux64
+cp pullAllRepos.sh ../Cuis-University-Installer/windows32
+cp pullAllRepos.sh ../Cuis-University-Installer/windows64
+cp pullAllRepos.sh ../Cuis-University-Installer/macos64
+
+cp clonePackageRepos.sh ../Cuis-University-Installer/linux32
+cp clonePackageRepos.sh ../Cuis-University-Installer/linux64
+cp clonePackageRepos.sh ../Cuis-University-Installer/windows32
+cp clonePackageRepos.sh ../Cuis-University-Installer/windows64
+cp clonePackageRepos.sh ../Cuis-University-Installer/macos64
+
 cp -R Packages ../Cuis-University-Installer/linux32/Packages
 cp -R Packages ../Cuis-University-Installer/linux64/Packages
 cp -R Packages ../Cuis-University-Installer/windows32/Packages
 cp -R Packages ../Cuis-University-Installer/windows64/Packages
 cp -R Packages ../Cuis-University-Installer/macos64/Packages
 
-cp ../Measures/Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/linux32/Packages
-cp ../Measures/Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/linux64/Packages
-cp ../Measures/Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/windows32/Packages
-cp ../Measures/Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/windows64/Packages
-cp ../Measures/Aconcagua/Aconcagua.pck.st ../Cuis-University-Installer/macos64/Packages
+rsync -r --exclude '.git*' ../Measures ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../Measures ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../Measures ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../Measures ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../Measures ../Cuis-University-Installer/macos64/Packages
 
-cp ../Calendars/Chalten/Chalten.pck.st ../Cuis-University-Installer/linux32/Packages
-cp ../Calendars/Chalten/Chalten.pck.st ../Cuis-University-Installer/linux64/Packages
-cp ../Calendars/Chalten/Chalten.pck.st ../Cuis-University-Installer/windows32/Packages
-cp ../Calendars/Chalten/Chalten.pck.st ../Cuis-University-Installer/windows64/Packages
-cp ../Calendars/Chalten/Chalten.pck.st ../Cuis-University-Installer/macos64/Packages
+rsync -r --exclude '.git*' ../Calendars ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../Calendars ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../Calendars ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../Calendars ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../Calendars ../Cuis-University-Installer/macos64/Packages
 
-cp ../LiveTyping/Smalltalk/LiveTyping.pck.st ../Cuis-University-Installer/linux32/Packages
-cp ../LiveTyping/Smalltalk/LiveTyping.pck.st ../Cuis-University-Installer/linux64/Packages
-cp ../LiveTyping/Smalltalk/LiveTyping.pck.st ../Cuis-University-Installer/windows32/Packages
-cp ../LiveTyping/Smalltalk/LiveTyping.pck.st ../Cuis-University-Installer/windows64/Packages
-cp ../LiveTyping/Smalltalk/LiveTyping.pck.st ../Cuis-University-Installer/macos64/Packages
+mkdir ../Cuis-University-Installer/linux32/Packages/LiveTyping
+mkdir ../Cuis-University-Installer/linux64/Packages/LiveTyping
+mkdir ../Cuis-University-Installer/windows32/Packages/LiveTyping
+mkdir ../Cuis-University-Installer/windows64/Packages/LiveTyping
+mkdir ../Cuis-University-Installer/macos64/Packages/LiveTyping
 
-cp ../Cuis-Smalltalk-DenotativeObject/DenotativeObject.pck.st ../Cuis-University-Installer/linux32/Packages
-cp ../Cuis-Smalltalk-DenotativeObject/DenotativeObject.pck.st ../Cuis-University-Installer/linux64/Packages
-cp ../Cuis-Smalltalk-DenotativeObject/DenotativeObject.pck.st ../Cuis-University-Installer/windows32/Packages
-cp ../Cuis-Smalltalk-DenotativeObject/DenotativeObject.pck.st ../Cuis-University-Installer/windows64/Packages
-cp ../Cuis-Smalltalk-DenotativeObject/DenotativeObject.pck.st ../Cuis-University-Installer/macos64/Packages
+rsync -r --exclude '.git*' ../LiveTyping/Smalltalk/*.pck.st ../Cuis-University-Installer/linux32/Packages/LiveTyping
+rsync -r --exclude '.git*' ../LiveTyping/Smalltalk/*.pck.st ../Cuis-University-Installer/linux64/Packages/LiveTyping
+rsync -r --exclude '.git*' ../LiveTyping/Smalltalk/*.pck.st ../Cuis-University-Installer/windows32/Packages/LiveTyping
+rsync -r --exclude '.git*' ../LiveTyping/Smalltalk/*.pck.st ../Cuis-University-Installer/windows64/Packages/LiveTyping
+rsync -r --exclude '.git*' ../LiveTyping/Smalltalk/*.pck.st ../Cuis-University-Installer/macos64/Packages/LiveTyping
 
-cp ../Cuis-Smalltalk-Refactoring/Refactoring.pck.st ../Cuis-University-Installer/linux32/Packages
-cp ../Cuis-Smalltalk-Refactoring/Refactoring.pck.st ../Cuis-University-Installer/linux64/Packages
-cp ../Cuis-Smalltalk-Refactoring/Refactoring.pck.st ../Cuis-University-Installer/windows32/Packages
-cp ../Cuis-Smalltalk-Refactoring/Refactoring.pck.st ../Cuis-University-Installer/windows64/Packages
-cp ../Cuis-Smalltalk-Refactoring/Refactoring.pck.st ../Cuis-University-Installer/macos64/Packages
+rsync -r --exclude '.git*' ../Cuis-Smalltalk-DenotativeObject ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../Cuis-Smalltalk-DenotativeObject ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../Cuis-Smalltalk-DenotativeObject ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../Cuis-Smalltalk-DenotativeObject ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../Cuis-Smalltalk-DenotativeObject ../Cuis-University-Installer/macos64/Packages
 
-cp ../Cuis-University/Cuis-University-Model.pck.st ../Cuis-University-Installer/linux32/Packages
-cp ../Cuis-University/Cuis-University-Model.pck.st ../Cuis-University-Installer/linux64/Packages
-cp ../Cuis-University/Cuis-University-Model.pck.st ../Cuis-University-Installer/windows32/Packages
-cp ../Cuis-University/Cuis-University-Model.pck.st ../Cuis-University-Installer/windows64/Packages
-cp ../Cuis-University/Cuis-University-Model.pck.st ../Cuis-University-Installer/macos64/Packages
+rsync -r --exclude '.git*' ../Cuis-Smalltalk-Refactoring ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../Cuis-Smalltalk-Refactoring ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../Cuis-Smalltalk-Refactoring ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../Cuis-Smalltalk-Refactoring ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../Cuis-Smalltalk-Refactoring ../Cuis-University-Installer/macos64/Packages
 
-cp ../Cuis-University/Cuis-University-Tests.pck.st ../Cuis-University-Installer/linux32/Packages
-cp ../Cuis-University/Cuis-University-Tests.pck.st ../Cuis-University-Installer/linux64/Packages
-cp ../Cuis-University/Cuis-University-Tests.pck.st ../Cuis-University-Installer/windows32/Packages
-cp ../Cuis-University/Cuis-University-Tests.pck.st ../Cuis-University-Installer/windows64/Packages
-cp ../Cuis-University/Cuis-University-Tests.pck.st ../Cuis-University-Installer/macos64/Packages
+rsync -r --exclude '.git*' ../Cuis-University ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../Cuis-University ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../Cuis-University ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../Cuis-University ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../Cuis-University ../Cuis-University-Installer/macos64/Packages
 
-cp ../TDDGuru/*.pck.st ../Cuis-University-Installer/linux32/Packages
-cp ../TDDGuru/*.pck.st ../Cuis-University-Installer/linux64/Packages
-cp ../TDDGuru/*.pck.st ../Cuis-University-Installer/windows32/Packages
-cp ../TDDGuru/*.pck.st ../Cuis-University-Installer/windows64/Packages
-cp ../TDDGuru/*.pck.st ../Cuis-University-Installer/macos64/Packages
+rsync -r --exclude '.git*' ../TDDGuru ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../TDDGuru ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../TDDGuru ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../TDDGuru ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../TDDGuru ../Cuis-University-Installer/macos64/Packages
 
-cp ../Cuis-Smalltalk-Refactoring/*.pck.st ../Cuis-University-Installer/linux32/Packages
-cp ../Cuis-Smalltalk-Refactoring/*.pck.st ../Cuis-University-Installer/linux64/Packages
-cp ../Cuis-Smalltalk-Refactoring/*.pck.st ../Cuis-University-Installer/windows32/Packages
-cp ../Cuis-Smalltalk-Refactoring/*.pck.st ../Cuis-University-Installer/windows64/Packages
-cp ../Cuis-Smalltalk-Refactoring/*.pck.st ../Cuis-University-Installer/macos64/Packages
+rsync -r --exclude '.git*' ../cuis-finder-asWidget ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../cuis-finder-asWidget ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../cuis-finder-asWidget ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../cuis-finder-asWidget ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../cuis-finder-asWidget ../Cuis-University-Installer/macos64/Packages
 
-#cp ../Cuis-Smalltalk-Refactorings-Thesis/*.pck.st ../Cuis-University-Installer/linux32/Packages
-#cp ../Cuis-Smalltalk-Refactorings-Thesis/*.pck.st ../Cuis-University-Installer/linux64/Packages
-#cp ../Cuis-Smalltalk-Refactorings-Thesis/*.pck.st ../Cuis-University-Installer/windows32/Packages
-#cp ../Cuis-Smalltalk-Refactorings-Thesis/*.pck.st ../Cuis-University-Installer/windows64/Packages
-#cp ../Cuis-Smalltalk-Refactorings-Thesis/*.pck.st ../Cuis-University-Installer/macos64/Packages
+rsync -r --exclude '.git*' ../cuis-code-coverage ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../cuis-code-coverage ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../cuis-code-coverage ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../cuis-code-coverage ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../cuis-code-coverage ../Cuis-University-Installer/macos64/Packages
+
+rsync -r --exclude '.git*' ../cuis-system-updater ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../cuis-system-updater ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../cuis-system-updater ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../cuis-system-updater ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../cuis-system-updater ../Cuis-University-Installer/macos64/Packages
+
+rsync -r --exclude '.git*' ../VectorGraphics ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../VectorGraphics ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../VectorGraphics ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../VectorGraphics ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../VectorGraphics ../Cuis-University-Installer/macos64/Packages
+
+rsync -r --exclude '.git*' ../TheCuisBook ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../TheCuisBook ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../TheCuisBook ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../TheCuisBook ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../TheCuisBook ../Cuis-University-Installer/macos64/Packages
+
+rsync -r --exclude '.git*' ../Numerics ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../Numerics ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../Numerics ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../Numerics ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../Numerics ../Cuis-University-Installer/macos64/Packages
+
+rsync -r --exclude '.git*' ../Erudite ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../Erudite ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../Erudite ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../Erudite ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../Erudite ../Cuis-University-Installer/macos64/Packages
+
+rsync -r --exclude '.git*' ../Games ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../Games ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../Games ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../Games ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../Games ../Cuis-University-Installer/macos64/Packages
+
+rsync -r --exclude '.git*' ../Morphic ../Cuis-University-Installer/linux32/Packages
+rsync -r --exclude '.git*' ../Morphic ../Cuis-University-Installer/linux64/Packages
+rsync -r --exclude '.git*' ../Morphic ../Cuis-University-Installer/windows32/Packages
+rsync -r --exclude '.git*' ../Morphic ../Cuis-University-Installer/windows64/Packages
+rsync -r --exclude '.git*' ../Morphic ../Cuis-University-Installer/macos64/Packages
 
 cp -R Documentation ../Cuis-University-Installer/linux32/Documentation
 cp -R Documentation ../Cuis-University-Installer/linux64/Documentation
@@ -154,11 +224,11 @@ cp -R CoreUpdates ../Cuis-University-Installer/windows32/CoreUpdates
 cp -R CoreUpdates ../Cuis-University-Installer/windows64/CoreUpdates
 cp -R CoreUpdates ../Cuis-University-Installer/macos64/CoreUpdates
 
-cp -R AdditionalFontData ../Cuis-University-Installer/linux32/AdditionalFontData
-cp -R AdditionalFontData ../Cuis-University-Installer/linux64/AdditionalFontData
-cp -R AdditionalFontData ../Cuis-University-Installer/windows32/AdditionalFontData
-cp -R AdditionalFontData ../Cuis-University-Installer/windows64/AdditionalFontData
-cp -R AdditionalFontData ../Cuis-University-Installer/macos64/AdditionalFontData
+cp -R TrueTypeFonts ../Cuis-University-Installer/linux32/TrueTypeFonts
+cp -R TrueTypeFonts ../Cuis-University-Installer/linux64/TrueTypeFonts
+cp -R TrueTypeFonts ../Cuis-University-Installer/windows32/TrueTypeFonts
+cp -R TrueTypeFonts ../Cuis-University-Installer/windows64/TrueTypeFonts
+cp -R TrueTypeFonts ../Cuis-University-Installer/macos64/TrueTypeFonts
 
 cd ../Cuis-University-Installer
 ../Squeak.app/Contents/MacOS/Squeak InstallerBuilder.image -d 'CuisInstallerBuilder generateScriptsAndExit'
